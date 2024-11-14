932a0ed4d0cdee0ae4129605396a56d68a2525ca
Stella Yang <43301578+lilacstella@users.noreply.github.com>
2024-11-14 01:00:55 -0600

    implement regex tree

--- regex_tree.py
+++ regex_tree.py
@@ -1,3 +1,18 @@
 class RegexTree:
-    pass
+    def __init__(self, root):
+        self.root = root
+        self.content = None
+        self.cost = -1
 
+    def get_cost(self) -> int:
+        if self.cost == -1:
+            self.cost = self.root.calculate_cost()
+        return self.cost
+
+    def get_content(self) -> str:
+        if not self.content:
+            self.content = self.root.interpret_content()
+        return self.content
+
+    def get_root(self):
+        return self.root
