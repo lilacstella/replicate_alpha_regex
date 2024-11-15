352d4283d503d41ec3124f94d7f6602e366dbb6a
Stella Yang <43301578+lilacstella@users.noreply.github.com>
2024-11-14 20:24:01 -0600

    use new deep copy

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 import heapq
 import re
 import collections
+import copy
 from regex_tree import RegexTree
 from regex_node import RegexNode
 
@@ -8,61 +9,60 @@ from regex_node import RegexNode
 # N = ["1", "10", "11", "100", "101"]
 
 class GenerateRegex:
-    def __init__(self, P, N):
-        self.P = P
-        self.N = N
+    def __init__(self, p, n):
+        self.P = p
+        self.N = n
 
     def solution(self, s: RegexTree):
-        if "☐" in s.content:
+        if "☐" in s.get_content():
             return False
 
         for p in self.P:
-            if not re.fullmatch(s.content, p):
-                # print(f"{s.content} did not match P")
+            if not re.fullmatch(s.get_content(), p):
                 return False
         for n in self.N:
-            if re.fullmatch(s.content, n):
-                # print(f"{s.content} matched N")
+            if re.fullmatch(s.get_content(), n):
                 return False
+
         return True
 
     def next_state(self, s: RegexTree):
-        if "☐" not in s.content:
+        if "☐" not in s.get_content():
             return []
+
         # next(s) = { s' | s -> s'}
         # if it makes it here this state already failed
         # apply all the implied operations upon all holes
+        # and discard it after we extract all its states
         output = []
         # print(f"generating new states from \n{s.content}")
-        original_tree = s.deep_copy()
-        queue = collections.deque([original_tree.root])
+        clone_root = s.get_root().deepcopy()
+
+        queue = collections.deque([clone_root])
         while queue:
             node = queue.popleft()
             if node.value == "☐":
                 for replacement in ["0", "1", "ε", "∅", "."]:
                     # currently I have a reference to the original tree node with a hole
                     node.value = replacement
-                    output.append(original_tree.deep_copy())
+                    output.append(copy.deepcopy(clone_root))
                 node.value = "*"
                 node.children = [RegexNode("☐")]
-                output.append(original_tree.deep_copy())
+                output.append(copy.deepcopy(clone_root))
                 node.children.append(RegexNode("☐"))
                 for replacement in ["∪", "⋅"]:
                     node.value = replacement
-                    output.append(original_tree.deep_copy())
+                    output.append(copy.deepcopy(clone_root))
                 node.value = "☐"
                 node.children = []
             else:
                 queue.extend(node.children)
-            # print(queue)
-        # print(output)
-        # also kills all dead states and redundant states
-        # for state in output:
-            # print(state.content.replace("☐", ".*"))
-            # re.fullmatch(state.content.replace("☐", ".*"), p) for p in self.P:
 
-        matches_all_patterns = lambda state: all(re.fullmatch(state.content.replace("☐", ".*"), p) for p in self.P)
+        # kill dead states
+        matches_all_patterns = lambda state: all(re.fullmatch(state.content.replace("☐", "(.*)"), p) for p in self.P)
         output = [state for state in output if matches_all_patterns(state)]
+
+        # generate and narrow redundant states
         # return an iterable of states
         # print("------------------------")
         # for i in output:
@@ -70,15 +70,15 @@ class GenerateRegex:
         return output
 
     def search_algorithm(self):
-        W: list = [RegexTree("☐")]  # Priority queue
-        while W:
+        w: list = [RegexTree("☐")]  # Priority queue
+        while w:
             # Get the next element from the priority queue
-            s: RegexTree = heapq.heappop(W)
+            s: RegexTree = heapq.heappop(w)
 
             # it should fail the states with holes and let next_state fill them in
             if self.solution(s):
                 return s
             for potential_state in self.next_state(s):
-                heapq.heappush(W, potential_state)
+                heapq.heappush(w, potential_state)
 
         return None
