92a59c67f77ed0c4cbe26e2e0fe09d2c2f4df3c2
Stella Yang <stella@stellayang.dev>
2025-03-31 00:19:31 -0500

    dump configs to results.txt

--- alpha_regex/__main__.py
+++ alpha_regex/__main__.py
@@ -1,13 +1,18 @@
+from alpha_regex import COST_MAP, benchmark_to_run, time_to_run
 from alpha_regex.benchmarks_runner import run_benchmark, run_all_benchmarks
 
 def main():
     reset = True
     if reset:
-        with open('results.txt', 'w') as _:
-            pass
+        with open('results.txt', 'w') as f:
+            f.write(f'{COST_MAP=}\n')
+            f.write(f'{time_to_run=} seconds\n')
+            f.write(f'{benchmark_to_run=}\n\n\n')
 
-    # run_benchmark('no01_start_with_a')
-    run_all_benchmarks()
+    if benchmark_to_run == 'all':
+        run_all_benchmarks()
+    else:
+        run_benchmark(benchmark_to_run)
 
 if __name__ == '__main__':
     main()
