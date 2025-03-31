a5b4138c043483fa0ceef0df0a47d761c2b1eb94
Stella Yang <stella@stellayang.dev>
2025-03-31 11:44:04 -0500

    Update search.py

--- alpha_regex/search.py
+++ alpha_regex/search.py
@@ -3,7 +3,7 @@ import re
 import collections
 import copy
 import time
-from alpha_regex import alphabet
+from alpha_regex import alphabet, time_to_run
 from alpha_regex.pattern import Pattern, Union, Concatenation, Star, Symbol, Box
 
 
@@ -29,9 +29,6 @@ class GenerateRegex:
         :return: whether the state is a solution or not
         """
         # save some effort str-ifying the pattern
-        if pattern.contains_box():
-            return False
-
         for p in self.P:
             if not re.fullmatch(str(pattern), p):
                 return False
@@ -58,16 +55,20 @@ class GenerateRegex:
     def kill_redundant_states(self, states: list[Pattern]):
         def unroll(state):
             if isinstance(state, Star):
-                return state.members[0] + state.members[0] + state
+                return Concatenation.set_members([state.members[0], state.members[0], state])
+
             if isinstance(state, Symbol) or isinstance(state, Box):
                 return state
 
             # Union or Concatenation
-            return state.__class__(members=[unroll(member) for member in state.members])
+            return state.__class__.set_members([unroll(member) for member in state.members])
 
         def split(state) -> list[Pattern]:
             if isinstance(state, Box) or isinstance(state, Symbol) or isinstance(state, Star):
                 return [state]
+
+            if (isinstance(state, Concatenation) or isinstance(state, Union)) and len(state.members) == 1:
+                return split(state.members[0])
 
             mid = len(state.members) // 2
             if isinstance(state, Union):
@@ -86,17 +87,14 @@ class GenerateRegex:
 
         return [state for state in states if all(self.matches_all_positive(expansion) for expansion in split(unroll(state)))]
 
-    def next_state(self, rejected_pattern: Pattern):
-        if not rejected_pattern.contains_box():
-            return []
-
+    def next_state(self, src: Pattern):
         # next(s) = { s' | s -> s'}
         # if it makes it here this state already failed
         # apply all the implied operations upon all holes
         # and discard it after we extract all its states
         new_states = []
 
-        queue = collections.deque([rejected_pattern])
+        queue = collections.deque([src])
         while queue:
             pattern = queue.popleft()
             if isinstance(pattern, Symbol):
@@ -104,7 +102,7 @@ class GenerateRegex:
             if isinstance(pattern, Box):
                 for symbol in alphabet.union(set("ε∅.")):
                     pattern.set_future(Symbol(symbol))
-                    new_states.append(copy.deepcopy(rejected_pattern))
+                    new_states.append(copy.deepcopy(src))
 
                 for operation in [
                     Box() + Box(),
@@ -112,12 +110,13 @@ class GenerateRegex:
                     Box().star()
                 ]:
                     pattern.set_future(operation)
-                    new_states.append(copy.deepcopy(rejected_pattern))
+                    new_states.append(copy.deepcopy(src))
             else:
                 queue.extend(pattern.members)
-        
-        # return self.kill_redundant_states(self.kill_dead_states(new_states))
-        return [state for state in new_states]
+
+        return self.kill_redundant_states(self.kill_dead_states(new_states))
+        # return self.kill_dead_states(new_states)
+        # return new_states
 
     def search_algorithm(self):
         """
@@ -128,25 +127,20 @@ class GenerateRegex:
         """
         start_time = time.time()
         state_count = 0
-        explored = set()
         w: list = [Box()]  # Priority queue
         while w:
-            if time.time() - start_time > 120:
+            if time.time() - start_time > time_to_run:
                 return None, state_count
             # Get the next element from the priority queue
             s: Pattern = heapq.heappop(w)
-            # the empty lang is making this concatenation empty
+            state_count += 1
             if not s.contains_box():
                 s = s.simplify()
+                if self.is_solution(s):
+                    return s, state_count
+                continue
 
-            state_count += 1
-            # it should fail the states with holes and let next_state fill them in
-            if self.is_solution(s):
-                return s, state_count
             for potential_state in self.next_state(s):
-                if str(potential_state) in explored:
-                    continue
                 heapq.heappush(w, potential_state)
-                explored.add(str(potential_state))
 
         return None, state_count
